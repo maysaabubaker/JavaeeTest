d955803efc92670e0b72041a3551bbd5f00f8ea9
maysa <maysaabubaker@gmail.com>
2019-10-09 15:07:25 +0300

    first commit

---
+++ src/com/maysa/hi/HelloWorld.java
@@ -0,0 +1,46 @@
+package com.maysa.hi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+/**
+ * Servlet implementation class HelloWorld
+ */
+//@WebServlet({ "/HelloWorld", "/maysa" })
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+PrintWriter out=response.getWriter();
+Date today =new Date();
+out.println("<h1>Hello World</h1>");
+out.println("<br>"+today+"</br>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
